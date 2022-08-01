d0a82fd622941a61559a85a39e7ac6acebc18f61
Eric <rodri1er@me.com>
2022-08-01 06:49:58 -0400

    Initial Commit

---
+++ BlackLotus/BlackLotusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackLotusApp.swift
+//  BlackLotus
+//
+//  Created by Eric Rodriguez on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BlackLotusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
